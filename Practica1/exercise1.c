87d469663e36016f1408c88bc64147af6a2956c1
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2020-10-15 10:56:08 +0200

    Apartado 4 a revisar...

--- Practica1/exercise1.c
+++ Practica1/exercise1.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 // Función que devuelve los segundos y microsegundos de su llamada
 // además, devuelve el tiempo de ejecución transcurrido entre la llamada anterior
@@ -23,23 +24,35 @@ void get_time (struct timeval current, long int *s, long int *us, double *time_e
 // Función a ejecutar en el hilo. Imprime el id del thread.
 void *threadfunction (void *arg)
 {
-    // Inicializaciones temporales
-    struct timeval current_time;
-    long int seconds, u_seconds;
-    double time_exe = 0;
-    // Capturamos el tiempo antes de la ejecución
-    get_time(current_time, &seconds, &u_seconds, &time_exe);
+    for (int it = 0; it < 3; it++){
+        // Inicializaciones temporales
+        struct timeval current_time;
+        long int seconds, u_seconds;
+        double time_exe = 0;
 
-    printf("Hello, I am a thread #%d.\n", *(int *)arg);
-    // Código random para que tarde en ejecutarse
-    volatile unsigned long long i;
-    for (i =0; i < 1000000000ULL; i++);
-    // Fin de la ejecución del código random
-    printf("Bye! Thread #%d with number.\n", *(int *)arg);
+        // Capturamos el tiempo antes de la ejecución
+        get_time(current_time, &seconds, &u_seconds, &time_exe);
 
-    // Capturamos el tiempo DESPUES de la ejecución e imprimimos resultado
-    get_time(current_time, &seconds, &u_seconds, &time_exe);
-    printf("El tiempo transcurrido para #%d es de %lf s...\n",*(int *)arg,time_exe);
+        printf("Hello, I am a thread #%d.\n", *(int *)arg);
+        // Código random para que tarde en ejecutarse
+        volatile unsigned long long i;
+        for (i =0; i < 500000000ULL; i++);
+        // Fin de la ejecución del código random
+
+        // Capturamos el tiempo DESPUES de la ejecución e imprimimos resultado
+        get_time(current_time, &seconds, &u_seconds, &time_exe);
+
+        if (time_exe > 0.9){
+            printf("Violacion del ciclo de ejecución de #%d\n",*(int *)arg);
+        }else{
+            if (time_exe < 0.4){
+                usleep(400000-(time_exe*100000));
+                time_exe = 0.4;
+            }        
+            printf("Bye! Thread #%d with number.\n", *(int *)arg);  
+        }
+        printf("El tiempo transcurrido para #%d es de %lf s...\n",*(int *)arg,time_exe);
+    }
     return 0;
 }
 
@@ -49,6 +62,12 @@ int main (void)
     pthread_t thread1, thread2, thread3, thread4;
     // Creamos los argumentos para pasar a los hilos
     int arg1 = 1, arg2 = 2, arg3 = 3, arg4 = 4;
+    // Inicializaciones temporales
+    struct timeval current_main_time;
+    long int main_seconds, main_u_seconds;
+    double main_time_exe = 0;
+
+    get_time(current_main_time, &main_seconds, &main_u_seconds, &main_time_exe);
     
     // Creamos los hilos pthread_create(a,b,c,d)
     //      a) &thread_id       -> puntero al id del hilo
@@ -66,4 +85,7 @@ int main (void)
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
     pthread_join(thread4, NULL);
+
+    get_time(current_main_time, &main_seconds, &main_u_seconds, &main_time_exe);
+    printf("Time to complete execution: %lf\n",main_time_exe);
 }
